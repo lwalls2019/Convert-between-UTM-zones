57c83c3956253b4d74ecca61bdeb02f1562e54c2
Liz <lwalls@oneenergyllc.com>
2019-10-30 09:29:22 -0400

    Open-source

--- Convert between UTM zones/UTM_Zone_Conversion.Designer.cs
+++ Convert between UTM zones/UTM_Zone_Conversion.Designer.cs
@@ -42,50 +42,58 @@
             this.label7 = new System.Windows.Forms.Label();
             this.txtUTMY = new System.Windows.Forms.TextBox();
             this.txtUTMX = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbo_North_or_South = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Location = new System.Drawing.Point(8, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(559, 37);
+            this.label1.Size = new System.Drawing.Size(377, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Convert Lat/Long to UTM Coordinates";
             // 
             // btnFindCoords
             // 
-            this.btnFindCoords.Location = new System.Drawing.Point(417, 192);
+            this.btnFindCoords.Location = new System.Drawing.Point(271, 110);
+            this.btnFindCoords.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnFindCoords.Name = "btnFindCoords";
-            this.btnFindCoords.Size = new System.Drawing.Size(120, 62);
+            this.btnFindCoords.Size = new System.Drawing.Size(109, 53);
             this.btnFindCoords.TabIndex = 1;
             this.btnFindCoords.Text = "Find UTM Coordinates";
             this.btnFindCoords.UseVisualStyleBackColor = true;
+            this.btnFindCoords.Click += new System.EventHandler(this.btnFindCoords_Click);
             // 
             // txtLat
             // 
             this.txtLat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtLat.Location = new System.Drawing.Point(249, 152);
+            this.txtLat.Location = new System.Drawing.Point(166, 99);
+            this.txtLat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLat.Name = "txtLat";
-            this.txtLat.Size = new System.Drawing.Size(100, 32);
+            this.txtLat.Size = new System.Drawing.Size(81, 24);
             this.txtLat.TabIndex = 2;
             // 
             // txtLong
             // 
             this.txtLong.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtLong.Location = new System.Drawing.Point(249, 205);
+            this.txtLong.Location = new System.Drawing.Point(166, 133);
+            this.txtLong.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLong.Name = "txtLong";
-            this.txtLong.Size = new System.Drawing.Size(100, 32);
+            this.txtLong.Size = new System.Drawing.Size(81, 24);
             this.txtLong.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(20, 152);
+            this.label2.Location = new System.Drawing.Point(13, 99);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(208, 26);
+            this.label2.Size = new System.Drawing.Size(140, 18);
             this.label2.TabIndex = 4;
             this.label2.Text = "Latitude (decimals) :";
             // 
@@ -93,27 +101,30 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 208);
+            this.label3.Location = new System.Drawing.Point(13, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(226, 26);
+            this.label3.Size = new System.Drawing.Size(153, 18);
             this.label3.TabIndex = 5;
             this.label3.Text = "Longitude (decimals) :";
             // 
             // txtUTMZone
             // 
             this.txtUTMZone.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUTMZone.Location = new System.Drawing.Point(249, 270);
+            this.txtUTMZone.Location = new System.Drawing.Point(166, 175);
+            this.txtUTMZone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUTMZone.Name = "txtUTMZone";
-            this.txtUTMZone.Size = new System.Drawing.Size(100, 32);
+            this.txtUTMZone.Size = new System.Drawing.Size(68, 24);
             this.txtUTMZone.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(20, 95);
+            this.label4.Location = new System.Drawing.Point(13, 62);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 32);
+            this.label4.Size = new System.Drawing.Size(65, 24);
             this.label4.TabIndex = 7;
             this.label4.Text = "Enter :";
             // 
@@ -121,9 +132,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(21, 273);
+            this.label5.Location = new System.Drawing.Point(14, 177);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(209, 26);
+            this.label5.Size = new System.Drawing.Size(144, 18);
             this.label5.TabIndex = 8;
             this.label5.Text = "UTM Zone Number :";
             // 
@@ -131,9 +143,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(23, 389);
+            this.label6.Location = new System.Drawing.Point(15, 253);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(92, 26);
+            this.label6.Size = new System.Drawing.Size(62, 18);
             this.label6.TabIndex = 12;
             this.label6.Text = "UTM Y :";
             // 
@@ -141,35 +154,63 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(23, 333);
+            this.label7.Location = new System.Drawing.Point(15, 216);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(91, 26);
+            this.label7.Size = new System.Drawing.Size(63, 18);
             this.label7.TabIndex = 11;
             this.label7.Text = "UTM X :";
             // 
             // txtUTMY
             // 
             this.txtUTMY.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUTMY.Location = new System.Drawing.Point(133, 383);
+            this.txtUTMY.Location = new System.Drawing.Point(89, 249);
+            this.txtUTMY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUTMY.Name = "txtUTMY";
             this.txtUTMY.ReadOnly = true;
-            this.txtUTMY.Size = new System.Drawing.Size(155, 32);
+            this.txtUTMY.Size = new System.Drawing.Size(105, 24);
             this.txtUTMY.TabIndex = 10;
             // 
             // txtUTMX
             // 
             this.txtUTMX.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUTMX.Location = new System.Drawing.Point(131, 333);
+            this.txtUTMX.Location = new System.Drawing.Point(87, 216);
+            this.txtUTMX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUTMX.Name = "txtUTMX";
             this.txtUTMX.ReadOnly = true;
-            this.txtUTMX.Size = new System.Drawing.Size(157, 32);
+            this.txtUTMX.Size = new System.Drawing.Size(106, 24);
             this.txtUTMX.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(15, 292);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Hemisphere :";
+            // 
+            // cbo_North_or_South
+            // 
+            this.cbo_North_or_South.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbo_North_or_South.FormattingEnabled = true;
+            this.cbo_North_or_South.Items.AddRange(new object[] {
+            "S",
+            "N"});
+            this.cbo_North_or_South.Location = new System.Drawing.Point(116, 289);
+            this.cbo_North_or_South.Name = "cbo_North_or_South";
+            this.cbo_North_or_South.Size = new System.Drawing.Size(78, 24);
+            this.cbo_North_or_South.TabIndex = 14;
             // 
             // UTM_Zone_Conversion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 559);
+            this.ClientSize = new System.Drawing.Size(402, 345);
+            this.Controls.Add(this.cbo_North_or_South);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.txtUTMY);
@@ -184,6 +225,7 @@
             this.Controls.Add(this.btnFindCoords);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UTM_Zone_Conversion";
             this.Text = "Converting between UTM zones";
             this.ResumeLayout(false);
@@ -206,6 +248,8 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txtUTMY;
         private System.Windows.Forms.TextBox txtUTMX;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbo_North_or_South;
     }
 }
 
--- Convert between UTM zones/UTM_Zone_Conversion.cs
+++ Convert between UTM zones/UTM_Zone_Conversion.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OSGeo.GDAL;
+using OSGeo.OGR;
+using OSGeo.OSR;
 
 namespace Convert_between_UTM_zones
 {
@@ -15,6 +18,76 @@ namespace Convert_between_UTM_zones
         public UTM_Zone_Conversion()
         {
             InitializeComponent();
+            cbo_North_or_South.SelectedIndex = 1;
+        }
+
+        private void btnFindCoords_Click(object sender, EventArgs e)
+        {
+            // Reads in latidue, longitude and UTM zone and calculates UTM coordinates
+
+            // first read in lat, long and UTM zone
+            float Lat = 0;
+            float Long = 0;
+            int UTM_Zone = 0;
+
+
+            try
+            {
+                Lat = Convert.ToSingle(txtLat.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid latitude.");
+                return;
+            }
+
+            try
+            {
+                Long = Convert.ToSingle(txtLong.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid longitude.");
+                return;
+            }
+
+            try
+            {
+                UTM_Zone = Convert.ToInt16(txtUTMZone.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid UTM zone.");
+                return;
+            }
+
+            // Now convert lat/long to UTM
+            Gdal.AllRegister();
+                        
+            // Create a spatial reference with geographic (lat/long) coordinates 
+
+            String Lat_Long_Proj_Str = "GEOGCS[\"NAD83\", DATUM[\"North_American_Datum_1983\", SPHEROID[\"GRS 1980\", 6378137, 298.257222101, AUTHORITY[\"EPSG\", \"7019\"]], TOWGS84[0, 0, 0, 0, 0, 0, 0], AUTHORITY[\"EPSG\", \"6269\"]], PRIMEM[\"Greenwich\", 0, AUTHORITY[\"EPSG\", \"8901\"]], UNIT[\"degree\", 0.0174532925199433, AUTHORITY[\"EPSG\", \"9108\"]], AUTHORITY[\"EPSG\", \"4269\"]]";
+            SpatialReference Lat_Long = new SpatialReference(Lat_Long_Proj_Str);
+
+            SpatialReference UTM_proj = new SpatialReference("");
+            UTM_proj.SetProjCS("UTM Proj");
+            UTM_proj.SetWellKnownGeogCS("NAD83");
+
+            int North_or_South = cbo_North_or_South.SelectedIndex;
+            
+            UTM_proj.SetUTM(UTM_Zone, North_or_South);
+
+            CoordinateTransformation ct = new CoordinateTransformation(Lat_Long, UTM_proj);
+            Double[] UTM_Point = new Double[3];
+            UTM_Point[0] = Long;
+            UTM_Point[1] = Lat;
+
+            ct.TransformPoint(UTM_Point);
+
+            txtUTMX.Text = Math.Round(UTM_Point[0],1).ToString();
+            txtUTMY.Text = Math.Round(UTM_Point[1],1).ToString();
+
+
         }
     }
 }
